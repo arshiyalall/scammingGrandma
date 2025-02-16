8d1f876337aecc827c42f235a04b473fb1e28869
strattonbd <strattonbd@gmail.com>
2025-02-16 18:23:58 -0500

    Basic room layout + player movement + wall colliders
    Updates to my scene and quality settings under the game tab have been switched from "Free Aspect" to "Full HD (1920 x 1080)"

---
+++ ScammingGrandmaTheGame/Assets/MoveSprite.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSprite : MonoBehaviour
+{
+    float moveSpeed = 4;
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            moveDirection(Vector2.right, 0, 1);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            moveDirection(Vector2.left, 0, -1);
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            moveDirection(Vector2.up, 1, 0);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            moveDirection(Vector2.down, -1, 0);
+        }
+        //Sprite is idle
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+    }
+
+    void moveDirection(Vector2 vector, float x, float y)
+    {
+        animator.SetBool("isWalking", true);
+        animator.SetFloat("InputX", x);
+        animator.SetFloat("InputY", y);
+        transform.Translate(vector * moveSpeed * Time.deltaTime);
+    }
+}
