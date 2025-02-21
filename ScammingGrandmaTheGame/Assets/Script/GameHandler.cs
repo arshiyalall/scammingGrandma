cc495235c01cb41742c7e64a829c0db3dda388ed
wbornmann <william.bornmann@tufts.edu>
2025-02-20 21:16:22 -0500

    GameHandler additions

--- ScammingGrandmaTheGame/Assets/Script/GameHandler.cs
+++ ScammingGrandmaTheGame/Assets/Script/GameHandler.cs
@@ -1,11 +1,111 @@
+using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class GameHandler : MonoBehaviour
-{
-    // Function to load the first scene in the build order
-    public void LoadFirstScene()
-    {
-        SceneManager.LoadScene(0); // Loads the first scene in Build Settings
+
+public class GameHandler : MonoBehaviour{
+
+    private GameObject player;
+    private string sceneName;
+    private int grandmaSatisfaction;
+
+    //information to gather
+    private bool gotSSN;
+    private bool gotMaiden;
+    private bool gotAddress;
+    private bool gotBankInfo;
+
+    private bool caughtByGuard;
+
+    private int numTasksCompleted;
+    private int numTasksAssigned;
+
+    // struct task {
+    //     string taskName;
+    //     bool taskCompleted;
+    // }
+
+    // private std::vector<task> taskArr;
+
+    void Start(){
+        SceneManager.LoadScene("StartScreen");
+        player = GameObject.FindWithTag("Player");
+        sceneName = SceneManager.GetActiveScene().name;
+           
     }
-}
+
+
+    //Each possible task function will be called if a bool is set to true.
+    //    This is so we can effectively choose which tasks the player has to 
+    //    complete for each day phase (and add more tasks if they get caught 
+    //    in night mode)
+
+
+    public void StartGame() {
+        SceneManager.LoadScene("Level1");
+
+        //set default values
+        gotSSN = false;
+        gotMaiden = false;
+        gotAddress = false;
+        gotBankInfo = false;
+        grandmaSatisfaction = 100;
+        numTasksCompleted = 0;
+        numTasksAssigned = 5;
+    }
+
+      // Return to MainMenu
+      public void RestartGame() {
+            Time.timeScale = 1f;
+            SceneManager.LoadScene("StartScreen");
+                // Please also reset all static variables here, for new games!
+      }
+
+
+    public void QuitGame() {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
+    }
+
+    //call this at the end of the day
+    //+1 task every day, +3 if you do not finish tasks
+    public void adjustNumTasks() {
+
+        //add 1 new task every day
+        numTasksAssigned += 1;
+
+        //2 extra tasks if caught by the guard
+        if (caughtByGuard) {
+            numTasksAssigned += 2;
+        }
+
+        //reset number of tasks completed
+        numTasksCompleted = 0;
+    }
+
+    public void incrementTasks(){
+        numTasksCompleted++;
+    }
+
+
+    //Functions for opening scenes. 
+    public void Settings() {
+        SceneManager.LoadScene("Settings");
+    }
+
+    public void Pause() {
+        //remember to pause timer. 
+        SceneManager.LoadScene("Pause");
+    }
+
+    public void Credits() {
+        SceneManager.LoadScene("Credits");
+    }
+}    
+
+
