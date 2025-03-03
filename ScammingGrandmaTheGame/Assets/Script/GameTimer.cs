e5e3dc690fa2d70beebdbce74935a9cdabed9c30
wbornmann <william.bornmann@tufts.edu>
2025-03-02 21:25:36 -0500

    Adding Timer

--- ScammingGrandmaTheGame/Assets/Script/GameTimer.cs
+++ ScammingGrandmaTheGame/Assets/Script/GameTimer.cs
@@ -2,14 +2,25 @@ using System.Collections.Generic;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 
 public class GameTimer : MonoBehaviour {
-    public int timer = 00;
+    public int timer = 60;
     private float theTimer = 0f;
     public GameObject timerText;
 
     void FixedUpdate(){
+        theTimer += 0.01f;
+        if (theTimer >= 1f){
+            timer -=1;
+            theTimer = 0;
+            UpdateTimer();
+        }
+            
+        if (timer <= 0) {
+        timerEnded();    
+        }
         theTimer += 0.01f;
         if (theTimer >= 1f){
             timer -=1;
@@ -29,5 +40,11 @@ public class GameTimer : MonoBehaviour {
 
     public void timerEnded(){
         UnityEngine.Debug.Log("Timer has reached 0");
+        
+        
+        //Here is the place where we will transition to the nightscreen. But it is not working. 
+        
+        SceneManager.LoadScene("NightScreen");
+
     }
 }
